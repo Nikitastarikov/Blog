6a46bd5282ddc6f9c9f271d56c71f710ad332761
Nikitastarikov <nikitka_starikov_99@mail.ru>
2020-12-26 17:14:38 +0700

    users + admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root 'posts#index'
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :posts, only: [:show, :index]
+	
+	namespace :admin do
+		resources :posts, except: [:show, :index]
+	end  
 end
