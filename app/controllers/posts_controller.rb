f1f31d7b3ad3dc324e83d1462cf505e1b545c69f
Nikitastarikov <nikitka_starikov_99@mail.ru>
2021-01-14 20:40:48 +0700

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,6 +47,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :summary, :body, :user_id, :draft)
+    params.require(:post).permit(:title, :summary, :body, :user_id, :draft, :image)
   end
 end
